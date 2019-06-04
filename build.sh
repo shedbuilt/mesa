baeff0908b62c2912980ca29ba7002ff72998164
Auston Stewart <auston@shedbuilt.net>
2019-06-03 18:12:38 -1000

    Updating to Mesa 19.1.0 RC4 and adding optional panfrost and lima driver support.

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
 SHED_PKG_LOCAL_GALLIUM_DRIVERS='swrast'
-if [ -n "${SHED_PKG_LOCAL_OPTIONS[panfrost]}" ]; then
+SHED_PKG_LOCAL_PLATFORMS='drm'
+# Configure
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[lima]}" ]; then
+    SHED_PKG_LOCAL_GALLIUM_DRIVERS="lima,kmsro"
+elif [ -n "${SHED_PKG_LOCAL_OPTIONS[panfrost]}" ]; then
     SHED_PKG_LOCAL_GALLIUM_DRIVERS="panfrost,kmsro"
+fi
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[wayland]}" ]; then
+    SHED_PKG_LOCAL_PLATFORMS='drm,wayland'
 fi
 # Build and Install
 mkdir build &&
@@ -13,9 +20,10 @@ meson --prefix=/usr                    \
       -Dvulkan-drivers=                \
       -Dgallium-drivers=${SHED_PKG_LOCAL_GALLIUM_DRIVERS} \
       -Dlibunwind=false                \
-      -Dplatforms=drm,wayland          \
+      -Dplatforms=${SHED_PKG_LOCAL_PLATFORMS} \
       -Dgbm=true                       \
-      -Dglx=disabled                      \
+      -Degl=true                       \
+      -Dglx=disabled                   \
       ..                              &&
 NINJAJOBS=$SHED_NUM_JOBS ninja &&
 DESTDIR="$SHED_FAKE_ROOT" ninja install || exit 1
